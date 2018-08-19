cbc13c623930e5d5b3c43841bb410afa3dcba816
enchom <encho.mishinev@leanplum.com>
2018-08-20 00:05:14 +0300

    more prints

--- uk/ac/cam/eim26/fjava/tick0/Resources.java
+++ uk/ac/cam/eim26/fjava/tick0/Resources.java
@@ -45,7 +45,7 @@ public class Resources {
                 minValue = Math.min(minValue, val);
                 maxValue = Math.max(maxValue, val);
 
-                if (val > 0 && iter < 1500) {
+                if (val > 0 && iter < 1500 && val < 1000000) {
                     System.out.println(val);
                     iter++;
                 }
