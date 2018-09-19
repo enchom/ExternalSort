c800d8a9a0c34bf5001fd86a0edbc98ead6789f1
unknown <encho.mishinev@gmail.com>
2018-09-20 02:15:39 +0300

    bucket sort time analytics

--- uk/ac/cam/eim26/fjava/tick0/ExternalBucketSort.java
+++ uk/ac/cam/eim26/fjava/tick0/ExternalBucketSort.java
@@ -4,9 +4,13 @@ import java.io.*; //TODO - split
 
 public class ExternalBucketSort extends ExternalBucketSortBase {
     public void sort() throws IOException {
+        long T1 = 0, T2 = 0, T3 = 0, T4 = 0, localTime;
+
         int len;
 
+        localTime = System.nanoTime();
         sortByFirstByte();
+        System.out.println("First byte sort is " + (System.nanoTime() - localTime)/1000000 + "ms");
 
         BufferedOutputStream firstFileStream = new BufferedOutputStream(new FileOutputStream(firstFile));
         FileInputStream d = new FileInputStream(secondFile); //TODO - More descriptive name
@@ -20,10 +24,13 @@ public class ExternalBucketSort extends ExternalBucketSortBase {
                 continue;
             }
 
+            localTime = System.nanoTime();
             d.read(arr, 0, len * 3);
+            T1 += System.nanoTime() - localTime;
 
             int realLen = len * 4;
 
+            localTime = System.nanoTime();
             for (int j = 3 * len - 3; j >= 0; j -= 3) {
                 arr[realLen-1] = arr[j+2];
                 arr[realLen-2] = arr[j+1];
@@ -31,11 +38,21 @@ public class ExternalBucketSort extends ExternalBucketSortBase {
                 arr[realLen-4] = (byte) realInd;
                 realLen -= 4;
             }
+            T2 += System.nanoTime() - localTime;
 
+            localTime = System.nanoTime();
             RadixByteSort.sortByteArray(arr, len, 1);
+            T3 += System.nanoTime() - localTime;
 
+            localTime = System.nanoTime();
             firstFileStream.write(arr, 0, len * 4);
+            T4 += System.nanoTime() - localTime;
         }
+
+        System.out.println("Reading " + T1/1000000 + "ms");
+        System.out.println("Converting " + T2/1000000 + "ms");
+        System.out.println("Sorting " + T3/1000000 + "ms");
+        System.out.println("Writing " + T4/1000000 + "ms");
 
         firstFileStream.close();
         d.close();
