49d6396cf373e68166b99a83547ff1158d8cbaa8
unknown <encho.mishinev@gmail.com>
2018-09-19 21:52:16 +0300

    print reduce

--- uk/ac/cam/eim26/fjava/tick0/Resources.java
+++ uk/ac/cam/eim26/fjava/tick0/Resources.java
@@ -65,13 +65,13 @@ public class Resources {
 
                 val = PartialByteHeapSort.bytesToInteger(arr, i / 4);
 
-                if (Resources.count[ arr[i]&0xff ] < 30 /*|| ( (arr[i]&0xff)==127 && fakectr < 30 && (arr[i+1] & 0xff) != 255 )*/) {
-                    System.out.println("Group " + (arr[i]&0xff) + " sees " + val + " with second bit " + (arr[i+1]&0xff) );
+                //if (Resources.count[ arr[i]&0xff ] < 30 /*|| ( (arr[i]&0xff)==127 && fakectr < 30 && (arr[i+1] & 0xff) != 255 )*/) {
+                    //System.out.println("Group " + (arr[i]&0xff) + " sees " + val + " with second bit " + (arr[i+1]&0xff) );
 
                     /*if ( (arr[i] & 0xff) == 127 && (arr[i+1] & 0xff) != 255 ) {
                         fakectr++;
                     }*/
-                }
+                //}
 
                 minValue = Math.min(minValue, val);
                 maxValue = Math.max(maxValue, val);
