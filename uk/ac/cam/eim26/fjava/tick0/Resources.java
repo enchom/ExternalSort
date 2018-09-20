6501b48ad92946bc169f734a0baeab50c9b0ba7f
unknown <encho.mishinev@gmail.com>
2018-09-20 14:58:31 +0300

    memory relaxation

--- uk/ac/cam/eim26/fjava/tick0/Resources.java
+++ uk/ac/cam/eim26/fjava/tick0/Resources.java
@@ -132,7 +132,7 @@ public class Resources {
 
         System.out.println("Memory = " + maxMemory + " i.e. " + ((double) (maxMemory) / 1000000.0) + "MB");
 
-        blockSize = (int) (usableMemory / 4);
+        blockSize = (int) (usableMemory / 5);
 
         System.out.println("Block size = " + blockSize);
     }
